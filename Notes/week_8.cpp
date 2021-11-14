bfe94d8cd99fc511889cb802342592798aa96743
Prachee Nanda <p4nanda@uwaterloo.ca>
2021-11-14 18:28:23 -0500

    Notes until 4.3

--- Notes/week_8.cpp
+++ Notes/week_8.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 //Introduction to Classes
 //Basic definition: Classes aggregate (combine elements) and protect data
 
@@ -26,15 +28,69 @@ public:
 };
 
 //4.02 Namespaces, std::cout and std::ostream
+//Everything in standard library is in the std namespace
+//std::sin etc. and few seldom exceptions assert() and M_PI
+
+//Create a namespace
+namespace ca_uwaterloo_p4nanda
+{
+    //some function in here
+}
+
+//4.03
+//Consider this vector 3D class
+//Let's try 2-norm
+//Sum of two vectors + scalar multiplication
+//Cross product and projection
+class Vector_3d;
+class Vector_3d
+{
+public:
+    double x_;
+    double y_;
+    double z_;
+};
+
+double norm(Vector_3d const &u);
+double inner_product(Vector_3d const &u, Vector_3d const &v);
+
+double inner_product(Vector_3d const &u, Vector_3d const &v)
+{
+    return u.x_ * v.x_ + u.y_ * v.y_ + u.z_ * v.z_;
+}
+double norm(Vector_3d const &u)
+{
+    return std::sqrt(inner_product(u, u))
+}
+
+//Array class
+//You can not create one because C++ needs to know the size at compile time
+//Classes can be dynamically allocated arrays
+
+//Pair class
+//When a function acts on a object, it is passed by reference
+//If we wanted more info returned, additional arguments need to be passed
+//Instead consider this
+class Pair;
+//function that returns a pair object
+Pair maxmin(Array const &array);
+//returns first and second value (RETURN TWO VALUES)
+class Pair
+{
+public:
+    std::size_t first_;
+    std::size_t second_;
+};
+//declare function here
 
 int main();
 int main()
 {
+    //4.01
     //Instance of class
-    Body earth{{149.6e6, 0, 0}, {0, 107e3, 0}, 5.972e24}
-        //Access member variables using .
-        std::cout
-        << earth.mass_ << std::endl;
+    Body earth{{149.6e6, 0, 0}, {0, 107e3, 0}, 5.972e24};
+    //Access member variables using .
+    std::cout << earth.mass_ << std::endl;
     std::cout << earth.position_[1] << std::endl;
     //Possible to mess around with them
     for (std::size_t k{0}; k < 3; ++k)
@@ -47,6 +103,10 @@ int main()
     //Objects can be passed as arguments
     //e.g. double speed (Body a); --> a = tmp, Earth
 
+    //4.02
+    std::clog << "This can be redirected to a log file" << std::endl;
+    //same as cout?
+    std::endl(std::cout << "TEST!"); //--> blows my mind
     return 0;
     //Memory for double is deallocated with return
 }
