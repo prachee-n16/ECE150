b585759e79425310f4a253ef342b5bf441803424
Prachee Nanda <p4nanda@uwaterloo.ca>
2021-11-14 18:00:19 -0500

    Week 8 notes till 4.1

--- Notes/week_8.cpp
+++ Notes/week_8.cpp
@@ -1,35 +1,52 @@
-#include <iostream>
+//Introduction to Classes
+//Basic definition: Classes aggregate (combine elements) and protect data
 
-//4.01 Class Data Structures
-//Simulate three-body problem for stars
-//Each star has a position x, y, and z, velocity v_x, v_y, v_z and a mass m
-//Store information in a class instead of just having multiple arrays
+//4.1 The class data structure
+//Consider three-body problem: Determine motion of three celestial bodies
+//moving under no influence other mutual gravity
 
-class Body(const &a, const &b);
-int main();
-//refer to a and b and not some copy of it
-class Body(const &a, const &b)
-{ //Class names are capitalized
+//Class can contain all information to describe one star
+//Class names are capitalized; Member variable ends with underscore
+
+//Declaring a class:
+class Body;
+//Declaring a function that returns a body
+//Issue: everytime object is passed as argument, you have significant amount of copying
+Body center_of_mass(Body a, Body b);
+//So, use pass by constant reference
+Body center_of_mass(Body const &a, Body const &b);
+//Defining a class:
+class Body
+{
 public:
+    //Member variables
     double position_[3]; //km
     double velocity_[3]; //km/h
     double mass_;        //kg
-
-    //some for loop
-    //has all of the informatio needed to describe star
-
-    return position_;
 };
 
+//4.02 Namespaces, std::cout and std::ostream
+
+int main();
 int main()
 {
-    //create an instance for this class
-    Body earth{{150, 0.0, 0.0}, {0.0, 100, 0.0}, 5};
-    Body tmp{earth}; //stores earths values
-    tmp.mass_ = 0.4;
-    //objects can be passed as arguments
+    //Instance of class
+    Body earth{{149.6e6, 0, 0}, {0, 107e3, 0}, 5.972e24}
+        //Access member variables using .
+        std::cout
+        << earth.mass_ << std::endl;
+    std::cout << earth.position_[1] << std::endl;
+    //Possible to mess around with them
+    for (std::size_t k{0}; k < 3; ++k)
+    {
+        earth.position_[k] += 0.1 * earth.velocity_[k];
+    }
 
-    //access mass
-    //std::cout << earth.position_[0] << std::endl;
+    //Copy over earth entries to mars
+    Body mars{earth};
+    //Objects can be passed as arguments
+    //e.g. double speed (Body a); --> a = tmp, Earth
+
     return 0;
-};
+    //Memory for double is deallocated with return
+}
